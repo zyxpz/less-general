8ad73f90438df97b892c3a40f14f6437fac73575
wb-mxl231031 <wb-mxl231031@cainiao.com>
2018-10-25 16:51:04 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# less 的一些通用样式
+- 对移动端和pc做适配
+- 使用前先定义 @module：'pc' 或 @module: 'mobile'
+- 文档以后放出
---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+const gulp = require('gulp');
+const plugins = require('gulp-load-plugins')();
+const less = require('gulp-less');
+const stripCssComments = require('gulp-strip-css-comments');
+const LessAutoprefix = require('less-plugin-autoprefix');
+const autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
+
+const paths = {
+	css: 'lib',
+	dist: 'dist',
+	distCss: 'dist/'
+};
+
+const distFiles = {
+	css: [
+		paths.css + '/*.css',
+		paths.css + '/*.less'
+	]
+};
+
+const files = {
+	css: [
+		paths.css + "/*.css",
+		paths.css + "/*.less",
+		paths.css + "/**/*.css",
+		paths.css + "/**/*.less"
+	]
+};
+
+// 清理dist
+gulp.task('clean', () => {
+	return gulp.src(paths.dist)
+		.pipe(plugins.clean({
+			force: true
+		}));
+});
+
+// css, less
+gulp.task('styles', () => {
+	return gulp.src(distFiles.css)
+		.pipe(less({
+      plugins: [autoprefix]
+    }))
+		.pipe(stripCssComments())
+    .pipe(plugins.minifyCss({
+      advanced: false
+    }))
+    .pipe(gulp.dest(paths.distCss));
+})
+
+gulp.task("watch:css", function() {
+	// 监听css变化
+	gulp.watch(files.css, function() {
+		gulp.run('styles');
+	});
+});
+
+gulp.task('default', function() {
+	gulp.run('styles');
+	gulp.run('watch:css');
+});
+
+gulp.task('build', ['clean'], function() {
+	gulp.run('default');
+});
